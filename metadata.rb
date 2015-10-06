4f994a4cb2283f512d58855ca13b89329a594bca
BART NOWAK <bnowak@bnowak.com>
2015-10-06 12:57:17 +0100

    + fixed action => different angle

--- metadata.rb
+++ metadata.rb
@@ -23,7 +23,7 @@ maintainer_email 'bnowak@bnowak.com'
 license          'Apache 2.0'
 description      'Installs/Configures frontend-standard-stack'
 long_description 'Installs/Configures frontend-standard-stack'
-version          '0.1.8'
+version          '0.1.9'
 
 supports 'ubuntu'
 
